e422efdea72bad5788d1405e026c1a7de2c272ba
oldcast1e <kylhs0207@naver.com>
2024-03-22 02:44:23 +0900

    update

---
+++ Smarcle/Techtalk/p1.py
@@ -0,0 +1,2 @@
+import pandas as pd
+pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
---
+++ Smarcle/Kaggle/Learn/pandas/test.py
@@ -0,0 +1,2 @@
+import pandas as pd
+pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
